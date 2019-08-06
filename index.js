2fec0c19dc56e9d05e60838e95f760befabfa203
insideoutzombie <david@web3devs.com>
2019-08-06 15:54:49 -0500

    initial project

---
+++ index.js
@@ -0,0 +1,18 @@
+alert("Hello World");
+
+var nameVariable = "David the legend";
+console.log("Hello " + nameVariable);
+
+
+var dogAge = 2;
+console.log("Your dog is: " + dogAge * 7);
+
+var numb = 2;
+console.log("1 + 1 = " + numb);
+//     Problem 1 Print "Hello World"
+// // //
+// // // Problem 2 Create a variable called nameVariable. Print "Hello " + nameVariable
+// // //
+// // // Problem 3 Create a variable named dogAge. Print "Your dog is: " + dogAge * 7
+// // //
+// // // Problem 4 Print "1 + 1 = " and the answer.
